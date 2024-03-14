2c3caf5a9f4ad7eefeecb24033f5417386e378cc
Gerrit Niezen <info@embeddedloop.com>
2024-03-14 12:02:53 +0000

    switch back to other zig action; try building on ubuntu because why not

--- build.zig
+++ build.zig
@@ -4,7 +4,10 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "helper",
         .root_source_file = .{ .path = "helper.zig" },
-        .target = b.resolveTargetQuery(.{ .os_tag = .windows }),
+        .target = b.resolveTargetQuery(.{ 
+            .os_tag = .windows,
+            .cpu_arch = .x86_64,
+        }),
     });
 
     const version = b.option([]const u8, "version", "application version string") orelse "0.0.0";
